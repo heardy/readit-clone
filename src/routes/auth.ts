d058790dd04a45b535f5d7cb44bcb2e5283c270c
Kevin Heard <kevin.heard@travelctm.com>
2022-05-13 18:13:23 +1000

    Creating Comments & Fetching Posts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,12 @@
-import { Request, Response, Router } from "express";
-import { isEmpty, validate } from "class-validator";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import cookie from "cookie";
+import { Request, Response, Router } from 'express';
+import { isEmpty, validate } from 'class-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
-import { AppDataSource } from "../data-source";
-import User from "../entity/User";
-import auth from "../middleware/auth";
+import { AppDataSource } from '../data-source';
+import User from '../entity/User';
+import auth from '../middleware/auth';
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
@@ -18,8 +18,8 @@ const register = async (req: Request, res: Response) => {
     const emailUser = await userRepository.findOneBy({ email });
     const usernameUser = await userRepository.findOneBy({ username });
 
-    if (emailUser) errors.email = "Email is already taken";
-    if (usernameUser) errors.username = "Username is already taken";
+    if (emailUser) errors.email = 'Email is already taken';
+    if (usernameUser) errors.username = 'Username is already taken';
 
     if (Object.keys(errors).length > 0) {
       return res.status(400).json(errors);
@@ -46,36 +46,39 @@ const login = async (req: Request, res: Response) => {
   try {
     let errors: any = {};
 
-    if (isEmpty(username)) errors.username = "Username must not be empty";
-    if (isEmpty(password)) errors.password = "Password must not be empty";
+    if (isEmpty(username)) errors.username = 'Username must not be empty';
+    if (isEmpty(password)) errors.password = 'Password must not be empty';
 
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     const userRepository = AppDataSource.getRepository(User);
     const user = await userRepository.findOneBy({ username });
 
-    if (!user) res.status(404).json({ error: "User not found!" });
+    if (!user) return res.status(404).json({ error: 'User not found!' });
 
     const passswordMatches = await bcrypt.compare(password, user.password);
 
     if (!passswordMatches)
-      return res.status(401).json({ password: "Password is incorrect" });
+      return res.status(401).json({ password: 'Password is incorrect' });
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+    const token = jwt.sign({ username }, process.env.JWT_SECRET!);
 
     res.set(
-      "Set-Cookie",
-      cookie.serialize("token", token, {
+      'Set-Cookie',
+      cookie.serialize('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
         maxAge: 3600,
-        path: "/",
+        path: '/',
       })
     );
 
     return res.json(user);
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
 };
 
 const me = (_: Request, res: Response) => {
@@ -84,12 +87,12 @@ const me = (_: Request, res: Response) => {
 
 const logout = (_: Request, res: Response) => {
   res.set(
-    "Set-Cookie",
-    cookie.serialize("token", "", {
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+    'Set-Cookie',
+    cookie.serialize('token', '', {
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
       expires: new Date(0),
-      path: "/",
+      path: '/',
     })
   );
 
@@ -97,9 +100,9 @@ const logout = (_: Request, res: Response) => {
 };
 
 const router = Router();
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", auth, logout);
-router.get("/me", auth, me);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', auth, logout);
+router.get('/me', auth, me);
 
 export default router;
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { AppDataSource } from '../data-source';
+import Comment from '../entity/Comment';
 import Post from '../entity/Post';
 import Sub from '../entity/Sub';
 
@@ -30,7 +31,63 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
+const getPosts = async (_: Request, res: Response) => {
+  try {
+    const posts = await AppDataSource.getRepository(Post).find({
+      order: { createdAt: 'DESC' },
+    });
+
+    return res.json(posts);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+const getPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  try {
+    const post = await AppDataSource.getRepository(Post).findOneOrFail({
+      where: {
+        identifier,
+        slug,
+      },
+      relations: ['sub'],
+    });
+
+    return res.json(post);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ error: 'Post not found' });
+  }
+};
+
+const commentsOnPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  const { body } = req.body;
+  try {
+    const post = await AppDataSource.getRepository(Post).findOneOrFail({
+      where: {
+        identifier,
+        slug,
+      },
+      relations: ['sub'],
+    });
+
+    const comment = new Comment({ body, user: res.locals.user, post });
+    await comment.save();
+
+    return res.json(comment);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ error: 'Post not found' });
+  }
+};
+
 const router = Router();
 router.post('/', auth, createPost);
+router.get('/', getPosts);
+router.get('/:identifier/:slug', getPost);
+router.post('/:identifier/:slug/comments', auth, commentsOnPost);
 
 export default router;
