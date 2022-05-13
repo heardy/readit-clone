891ff0748fe42e683ba0d9a1549f0cae3bfdff20
Kevin Heard <kevin.heard@travelctm.com>
2022-05-13 11:16:05 +1000

    Login & Cookies

---
+++ src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
+import { AppDataSource } from '../data-source';
+import { User } from '../entity/User';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) throw new Error('Unauthenticated');
+
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
+
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneBy({ username });
+
+    if (!user) throw new Error('Unauthenticated');
+
+    res.locals.user = user;
+
+    return next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ error: 'Unauthenticated' });
+  }
+}
