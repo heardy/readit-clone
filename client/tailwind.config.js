bc38cf7c7f654c48590b5190177a05e50f547a8c
Kevin Heard <kevin.heard@travelctm.com>
2022-05-16 12:20:56 +1000

    Nextjs App Setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        70: '17.5rem'
+      }
+    },
+  },
+  plugins: [],
+}
