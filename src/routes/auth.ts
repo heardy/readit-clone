64b66f6db84f98abd9b022be5eb30933adb43950
Kevin Heard <kevin.heard@travelctm.com>
2022-05-16 23:42:21 +1000

    Register & Login Wired Pages

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import e, { Request, Response, Router } from 'express';
 import { isEmpty, validate } from 'class-validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -7,6 +7,13 @@ import cookie from 'cookie';
 import { AppDataSource } from '../data-source';
 import User from '../entity/User';
 import auth from '../middleware/auth';
+
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
@@ -29,7 +36,9 @@ const register = async (req: Request, res: Response) => {
 
     errors = await validate(user);
 
-    if (errors.length > 0) return res.status(400).json({ errors });
+    if (errors.length > 0) {
+      return res.status(400).json(mapErrors(errors));
+    }
 
     await user.save();
 
@@ -54,7 +63,7 @@ const login = async (req: Request, res: Response) => {
     const userRepository = AppDataSource.getRepository(User);
     const user = await userRepository.findOneBy({ username });
 
-    if (!user) return res.status(404).json({ error: 'User not found!' });
+    if (!user) return res.status(404).json({ username: 'User not found!' });
 
     const passswordMatches = await bcrypt.compare(password, user.password);
 
